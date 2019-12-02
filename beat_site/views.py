9d787a441739b308dc91c0940de87551ae7039ae
Leon Bi <leon@DN0a1feb9d.SUNet>
2019-12-01 18:17:46 -0800

    distributions

--- beat_site/views.py
+++ beat_site/views.py
@@ -16,6 +16,9 @@ from django.urls import reverse
 from django.template.defaultfilters import slugify
 import math
 from django.core import serializers
+import numpy as np
+from scipy import stats
+import scipy
 
 def get_z_array(self, **kwargs):
     data = []
@@ -125,62 +128,204 @@ def get_z_array(self, **kwargs):
         print(line)
     return result
 
+def getIndex(entry):
+    if entry <= 10:
+        return 0
+    elif entry > 10 and entry <= 20 :
+        return 1
+    elif entry > 20 and entry <= 30 :
+        return 2
+    elif entry > 30 and entry <= 40 :
+        return 3
+    elif entry > 40 and entry <= 50 :
+        return 4
+    elif entry > 50 and entry <= 60 :
+        return 5
+    elif entry > 60 and entry <= 70 :
+        return 6
+    elif entry > 70 and entry <= 80 :
+        return 7
+    elif entry > 80 and entry <= 90 :
+        return 8
+    elif entry > 90 and entry <= 100 :
+        return 9
+    elif entry > 100 and entry <= 110 :
+        return 10
+    elif entry > 110 and entry <= 120 :
+        return 11
+    elif entry > 120 and entry <= 130 :
+        return 12
+    elif entry > 130 and entry <= 140 :
+        return 13
+    elif entry > 140 and entry <= 150 :
+        return 14
+    elif entry > 150 and entry <= 160 :
+        return 15
+    elif entry > 160 and entry <= 170 :
+        return 16
+    elif entry > 170 and entry <= 180 :
+        return 17
+    elif entry > 180 and entry <= 190 :
+        return 18
+    elif entry > 190 and entry <= 200 :
+        return 19
+    elif entry > 200:
+        return 20
+
 def UpdateDist(request):
-    min_value = int(request.GET.get('min', None)) * 1000000000
-    max_value = int(request.GET.get('max', None)) * 1000000000
-    year = request.GET.get('year', None)
-    print(year)
-    if year != 'all':
-        queryset = Country.objects.filter(gdp__range=(min_value, max_value), year=year).values()
-    else:
-        queryset = Country.objects.filter(gdp__range=(min_value, max_value)).values()
-    data = list(queryset)
     year_data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for country in data:
-        entry = country['rate']
-        if entry <= 10:
-            year_data[0] += 1
-        elif entry > 10 and entry <= 20 :
-            year_data[1] += 1
-        elif entry > 20 and entry <= 30 :
-            year_data[2] += 1
-        elif entry > 30 and entry <= 40 :
-            year_data[3] += 1
-        elif entry > 40 and entry <= 50 :
-            year_data[4] += 1
-        elif entry > 50 and entry <= 60 :
-            year_data[5] += 1
-        elif entry > 60 and entry <= 70 :
-            year_data[6] += 1
-        elif entry > 70 and entry <= 80 :
-            year_data[7] += 1
-        elif entry > 80 and entry <= 90 :
-            year_data[8] += 1
-        elif entry > 90 and entry <= 100 :
-            year_data[9] += 1
-        elif entry > 100 and entry <= 110 :
-            year_data[10] += 1
-        elif entry > 110 and entry <= 120 :
-            year_data[11] += 1
-        elif entry > 120 and entry <= 130 :
-            year_data[12] += 1
-        elif entry > 130 and entry <= 140 :
-            year_data[13] += 1
-        elif entry > 140 and entry <= 150 :
-            year_data[14] += 1
-        elif entry > 150 and entry <= 160 :
-            year_data[15] += 1
-        elif entry > 160 and entry <= 170 :
-            year_data[16] += 1
-        elif entry > 170 and entry <= 180 :
-            year_data[17] += 1
-        elif entry > 180 and entry <= 190 :
-            year_data[18] += 1
-        elif entry > 190 and entry <= 200 :
-            year_data[19] += 1      
-        elif entry > 200:
-            year_data[20] += 1
-    return JsonResponse({'data':year_data})
+    sample_data = []
+    if request.GET.get('condition') == 'gdp':
+        min_value = float(request.GET.get('min', None)) * 1000000000
+        max_value = float(request.GET.get('max', None)) * 1000000000
+        year = request.GET.get('year', None)
+        print(year)
+        if year != 'all':
+            queryset = Country.objects.filter(gdp__range=(min_value, max_value), year=year).values()
+        else:
+            queryset = Country.objects.filter(gdp__range=(min_value, max_value)).values()
+        data = list(queryset)
+
+        for country in data:
+            entry = country['rate']
+            sample_data.append(entry)
+            #Organize so that we can graph distribution
+            if entry <= 10:
+                year_data[0] += 1
+            elif entry > 10 and entry <= 20 :
+                year_data[1] += 1
+            elif entry > 20 and entry <= 30 :
+                year_data[2] += 1
+            elif entry > 30 and entry <= 40 :
+                year_data[3] += 1
+            elif entry > 40 and entry <= 50 :
+                year_data[4] += 1
+            elif entry > 50 and entry <= 60 :
+                year_data[5] += 1
+            elif entry > 60 and entry <= 70 :
+                year_data[6] += 1
+            elif entry > 70 and entry <= 80 :
+                year_data[7] += 1
+            elif entry > 80 and entry <= 90 :
+                year_data[8] += 1
+            elif entry > 90 and entry <= 100 :
+                year_data[9] += 1
+            elif entry > 100 and entry <= 110 :
+                year_data[10] += 1
+            elif entry > 110 and entry <= 120 :
+                year_data[11] += 1
+            elif entry > 120 and entry <= 130 :
+                year_data[12] += 1
+            elif entry > 130 and entry <= 140 :
+                year_data[13] += 1
+            elif entry > 140 and entry <= 150 :
+                year_data[14] += 1
+            elif entry > 150 and entry <= 160 :
+                year_data[15] += 1
+            elif entry > 160 and entry <= 170 :
+                year_data[16] += 1
+            elif entry > 170 and entry <= 180 :
+                year_data[17] += 1
+            elif entry > 180 and entry <= 190 :
+                year_data[18] += 1
+            elif entry > 190 and entry <= 200 :
+                year_data[19] += 1      
+            elif entry > 200:
+                year_data[20] += 1
+
+
+    norm_mu, norm_sig, norm_likelihood = normal_mle(sample_data, year_data)
+    poiss_mu, poiss_likelihood = poisson_mle(sample_data, year_data)
+    pareto_a = pareto_mle(sample_data)
+    poiss_dist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    norm_dist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    pareto_dist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    trials = 1000
+
+    for i in range(trials):
+        entry = np.random.poisson(poiss_mu)
+        poiss_dist[getIndex(entry)] += 1
+
+    for i in range(trials):
+        entry = np.random.normal(norm_mu, norm_sig)
+        norm_dist[getIndex(entry)] += 1
+
+    for i in range(trials):
+        entry = np.random.pareto(pareto_a)
+        pareto_dist[getIndex(entry)] += 1
+
+
+    pdf_data = [value / sum(year_data) for value in year_data]
+    poiss_dist = [value / sum(poiss_dist) for value in poiss_dist]
+    norm_dist = [value / sum(norm_dist) for value in norm_dist]
+    pareto_dist = [value / sum(pareto_dist) for value in pareto_dist]
+
+
+    return JsonResponse({'data':pdf_data,
+                        'poiss_pdf':poiss_dist,
+                        'poiss_lambda':poiss_mu,
+                        'norm_pdf':norm_dist,
+                        'pareto_pdf':pareto_dist})
+
+
+def pareto_mle(sample_data):
+    n = len(sample_data)
+    p_sum= 0
+    k = min(sample_data)
+    for data in sample_data:
+        p_sum += math.log(data / k)
+    print(n / p_sum)
+    return n / p_sum
+
+def poisson_mle(sample_data, year_data):
+    data_sum = 0
+    poiss_pdf = []
+    likelihood = 1
+    for i in range(len(year_data)):
+        data_sum += year_data[i] * (i *10)
+
+    lamb = data_sum / sum(year_data)
+
+    for i in range(len(year_data)):
+       poiss_pdf.append(stats.poisson(lamb).pmf(i*10))
+
+    for data in sample_data:
+        likelihood *= stats.poisson(lamb).pmf(data)
+
+    return lamb, likelihood
+
+
+def normal_mle(sample_data, year_data):
+    # data_sum = 0
+    # for i in range(len(year_data)):
+    #     data_sum += year_data[i] * (i *10)
+    # mu = data_sum / sum(year_data)
+    mu = np.mean(sample_data)
+    sig = 0
+    likelihood = 1
+    norm_pdf = []
+    for data in sample_data:
+        sig += ((data - mu)**2)
+    sig /= len(sample_data)
+
+    for data in sample_data:
+        likelihood *= stats.norm(mu, sig).pdf(data)
+
+    # expected_values = []
+    # for i in range(len(year_data)):
+    #     data_point = stats.norm(mu, sig).pdf((i * 10) + 5)
+    #     expected_values.append(data_point)
+
+    # observed_values=scipy.array(year_data)
+
+    # print(stats.chisquare(observed_values, f_exp=expected_values))
+
+    for i in range(len(year_data)):
+       norm_pdf.append(stats.norm(mu, sig).pdf(i*10))
+
+
+    return mu, sig, likelihood
+
 
 class Graph(TemplateView):
     model = Country
